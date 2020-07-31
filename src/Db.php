64c206f92d5096f31be37eacf69d851b3db8189d
zmi zaytsev <zmigpost@gmail.com>
2020-07-31 16:30:19 +0200

    Remove g_config

--- src/Db.php
+++ src/Db.php
@@ -8,12 +8,25 @@
 class Db
 {
     private $databases = null;
+    public $config = [
+        'logDbInfo'  => false, // Логгировать ли все запросы? (по умолчанию посмотреть можно в DebugPanel, внизу сайта)
+        'logDbError' => true, // Логгировать запросы в которых произошла ошибка (по умолчанию в dbLogFile складываются)
+        'dbLogFile'  => null,
+
+        // Коннекты к базам данных
+        'databases'  => [
+            'db' => [
+                'dsn'        => 'mysqli://root:@localhost/dbName?charset=UTF8', // Подключение к БД в формате DSN-строки
+                // Кеш ф-я
+                // Перед запросом который в кеш отправиться нужно написать "-- CACHE: 1h 5m 15" затем ENTER. Здесь h/m/цифра это часы/минут/секунды соотвественно
+                'pCacheFunc' => ['MyDataBaseCache', 'Cache'],
+            ],
+        ],
+    ];
 
     // Обработчик ошибок БД
     public static function DbSimpleError($message, $info)
     {
-        global $g_config;
-
         if (!error_reporting()) {
             return;
         }
@@ -29,22 +42,32 @@ class Db
         }
 
         static $fileLogger = null;
-        if (is_null($fileLogger)) {
-            $fileLogger = FileLogger::Create($g_config['dbSimple']['dbLogFile']);
+
+        $self = Db::GetInstance();
+        $logPath = $self->config['dbLogFile'];
+
+        if ($logPath) {
+            if (is_null($fileLogger)) {
+                $fileLogger = FileLogger::Create($logPath);
+            }
+            $fileLogger->Error(
+                PHP_EOL .
+                "\tquery: " . $info['query'] . PHP_EOL .
+                "\tmessage: " . $info['message'] . PHP_EOL .
+                "\tcode: " . $info['code'] . PHP_EOL .
+                "\tcontext: " . $info['context'] . PHP_EOL .
+                PHP_EOL
+            );
+        } else {
+            throw new Exception($info['query']);
         }
-        $fileLogger->Error(
-            PHP_EOL .
-            "\tquery: " . $info['query'] . PHP_EOL .
-            "\tmessage: " . $info['message'] . PHP_EOL .
-            "\tcode: " . $info['code'] . PHP_EOL .
-            "\tcontext: " . $info['context'] . PHP_EOL .
-            PHP_EOL
-        );
     }
 
-    private function __construct($dbs)
+    private function __construct($config)
     {
-        global $g_config;
+        if (!empty($config)) {
+            $this->config = array_merge($this->config, $config);
+        }
 
         // Подключаем модули для работы с DbSimple (не по подгрузится автолоудером)
         $path = dirname(__FILE__) . '/DbSimple/';
@@ -56,18 +79,19 @@ class Db
 
         // Собираем все объекты в $o
         $o = new stdClass();
+        $dbs = $this->config['dbs'];
         foreach ($dbs as $db => $conn) {
             $dsn       = $conn['dsn'];
             $cacheFunc = isset($conn['pCacheFunc']) ? $conn['pCacheFunc'] : null;
 
             $o->$db = DbSimple_Generic::connect($dsn);
 
-            if ($g_config['dbSimple']['logDbError']) {
+            if ($this->config['logDbError']) {
                 MyDataBaseLog::SetFuncOnError([__CLASS__, 'DbSimpleError']);
                 $o->$db->setErrorHandler(['MyDataBaseLog', 'Error']);
             }
 
-            if ($g_config['dbSimple']['logDbInfo']) {
+            if ($this->config['logDbInfo']) {
                 $o->$db->setLogger(['MyDataBaseLog', 'Log']);
             }
 
